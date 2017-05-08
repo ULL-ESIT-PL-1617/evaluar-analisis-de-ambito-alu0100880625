f4b7076ff6bf8cf91ad09619ff7cdb76ffdbb265
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-05-08 14:22:39 +0100

    test/index.js

--- test/index.js
+++ test/index.js
@@ -1,17 +1,42 @@
 var should = require('chai').should();
+var { Node, Call, FunctionDef, BinOp, Comma, Leaf } = require('../node.js');
 var PEG = require("../grammar.js");
+var genCode = require("../gen-code.js");
 
-function requireFromString(src, filename) {
-  var Module = module.constructor;
-  var m = new Module();
-  m._compile(src, filename);
-  return m.exports;
-}
+describe('parser', function() {
+  it('builds the tree for a = 4', function() { 
+    var r = PEG.parse('a = 4');
+    let expected = new BinOp({
+                      type: '=',
+                      left: new Leaf({ type: 'ID', value: 'a' }),
+                      right: new Leaf({ type: 'NUM', value: '4' }) 
+                   });
+    r.should.deep.equal(expected); 
+  });
+});
 
-describe('translation', function() {
-  it('translates a = 4, b = 5+a, c = 2*a', function() { // change this test!
-    var r = PEG.parse('a = 4, b = 5+a, c = 2*a');
-    r.should.match(/a\s*=\s*4,\s*b\s*=\s*5[+]a,\s*c\s*=\s*2[*]a/); 
+describe('code generation', function() {
+  let compile = function(input) {
+    var r = PEG.parse(input);
+    var t = genCode(r);
+    return t;
+  };
+  let compileEval = function(input) {
+    var t = compile(input);
+    var x = eval(t);
+    return x();
+  };
+
+  it('translates  a = 4', function() { 
+    var r = compile('a = 4');
+    let expected =  /sym.a\s*=\s*4/;
+    r.should.match(expected); 
+  });
+
+  it('translates  a = 4, b = a + 1', function() { 
+    var r = compileEval('a = 4, b = a+1');
+    let expected = {a:4, b:5};
+    r.should.deep.equal(expected); 
   });
 });
 
